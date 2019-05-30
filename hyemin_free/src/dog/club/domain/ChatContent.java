df6a0c19a4e13206393b945dca507ccbefd279fb
HyeminNoh <shrmffkfk@naver.com>
2019-05-30 22:30:26 +0900

    방명록 객체 get,set 메소드

---
+++ hyemin_free/src/dog/club/domain/ChatContent.java
@@ -0,0 +1,29 @@
+package dog.club.domain;
+
+public class ChatContent {
+	private String id;
+	private String content;
+	private String date;
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
