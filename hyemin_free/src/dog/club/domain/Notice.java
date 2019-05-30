8dad340730fd51d747bce46acfb2843b96147f34
HyeminNoh <shrmffkfk@naver.com>
2019-05-30 22:31:18 +0900

    공지게시물 객체 get,set메소드

---
+++ hyemin_free/src/dog/club/domain/Notice.java
@@ -0,0 +1,41 @@
+package dog.club.domain;
+
+public class Notice {
+	private int no;
+	private String title;
+	private String id;
+	private String date;
+	private String content;
+	
+
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
